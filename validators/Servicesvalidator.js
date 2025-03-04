3ebfff1824fb2d181453a7c5af981e467f8607d9
aarzeal <168904553+aarzeal@users.noreply.github.com>
2025-03-04 15:40:40 +0530

    tax

--- validators/Servicesvalidator.js
+++ validators/Servicesvalidator.js
@@ -262,7 +262,7 @@ exports. finGroupValidationRules = [
       .withMessage("Types of group must be an integer."),
   
     body("is_primary_group")
-      .isInt({ min: 0, max: 1 })
+    .isBoolean()
       .withMessage("Primary group must be 0 or 1."),
   
     body("under_group_IDR")
