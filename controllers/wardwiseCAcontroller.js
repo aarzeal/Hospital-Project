5fee76c10ed0533f64e99d30a1e68d62613659d5
aarzeal <168904553+aarzeal@users.noreply.github.com>
2025-03-28 11:59:43 +0530

    ward wwca crud

--- controllers/wardwiseCAcontroller.js
+++ controllers/wardwiseCAcontroller.js
@@ -316,7 +316,7 @@ exports.createWwca = async (req, res) => {
       isCurrectRate,
       hospital_IDR,
       hospitalGroup_IDR,
-      createdBy,
+      createdBy: req.username,
       // updatedBy,
     });
 
@@ -345,7 +345,7 @@ exports.createWwca = async (req, res) => {
     const executionTime = `${Date.now() - start}ms`;
     const errorCode = 1262;
 
-    logger.logWithMeta("error", "Error creating Wwca record", {
+    logger.logWithMeta("error", "Error creating ward wise cost addition record", {
       errorCode,
       executionTime,
       hospitalId: req.hospitalName,
@@ -359,7 +359,7 @@ exports.createWwca = async (req, res) => {
 
     res.status(500).json({
       meta: { statusCode: 500, errorCode, executionTime, hospitalDatabase },
-      error: { message: "Error creating Wwca record: " + error.message },
+      error: { message: "Error creating ward wise cost addition record: " + error.message },
     });
   }
 };
@@ -371,7 +371,7 @@ exports.getWwca = async (req, res) => {
     const wwcaRecords = await Wwca.findAll();
 
     const executionTime = `${Date.now() - start}ms`;
-    logger.logWithMeta("info", "Fetched Wwca records successfully", {
+    logger.logWithMeta("info", "Fetched ward wise cost addition records successfully", {
       executionTime,
       hospitalId: req.hospitalName,
       apiName: req.originalUrl,
@@ -392,7 +392,7 @@ exports.getWwca = async (req, res) => {
     const executionTime = `${Date.now() - start}ms`;
     const errorCode = 1263;
 
-    logger.logWithMeta("error", "Error fetching Wwca records", {
+    logger.logWithMeta("error", "Error fetching ward wise cost addition records", {
       errorCode,
       executionTime,
       hospitalId: req.hospitalName,
@@ -403,18 +403,18 @@ exports.getWwca = async (req, res) => {
 
     res.status(500).json({
       meta: { statusCode: 500, errorCode, executionTime },
-      error: { message: "Error fetching Wwca records: " + error.message },
+      error: { message: "Error fetching ward wise cost addition records: " + error.message },
     });
   }
 
 }
 exports.getWwcaById = async (req, res) => {
   const start = Date.now();
-  const { id } = req.params;
+  const { Wwca_ID } = req.params;
 
   try {
     const Wwca = require("../models/wardwiseCAModel")(req.sequelize);
-    const wwcaRecord = await Wwca.findByPk(id);
+    const wwcaRecord = await Wwca.findByPk(Wwca_ID);
 
     if (!wwcaRecord) {
       return res.status(404).json({ message: "Wwca record not found" });
@@ -442,7 +442,7 @@ exports.getWwcaById = async (req, res) => {
     const executionTime = `${Date.now() - start}ms`;
     const errorCode = 1263;
 
-    logger.logWithMeta("error", "Error fetching Wwca records", {
+    logger.logWithMeta("error", "Error fetching ward wise cost addition records", {
       errorCode,
       executionTime,
       hospitalId: req.hospitalName,
@@ -453,7 +453,158 @@ exports.getWwcaById = async (req, res) => {
 
     res.status(500).json({
       meta: { statusCode: 500, errorCode, executionTime },
-      error: { message: "Error fetching Wwca records: " + error.message },
+      error: { message: "Error fetching ward wise cost addition records: " + error.message },
+    });
+  }
+}
+
+exports.updateWwcaById = async (req, res) => {
+  const errors = validationResult(req);
+  const start = Date.now();
+  const clientIp = await getClientIp(req);
+  const locationData = await getLocationData(clientIp);
+  const hospitalDatabase = req.hospitalDatabase;
+
+  const {
+    WwcatypeIDR,
+    typeEnum,
+    typeIDR,
+    costAddRate,
+    fromdate,
+    todate,
+    isActive,
+    isCurrectRate,
+    hospital_IDR,
+    hospitalGroup_IDR,
+    createdBy,
+    updatedBy,
+  } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const { Wwca_ID } = req.params;
+
+    const Wwca = require("../models/wardwiseCAModel.js")(req.sequelize);
+    const Hospital = require("../models/HospitalModel.js");
+    const HospitalGroup = require("../models/HospitalGroup.js");
+
+    const group = await HospitalGroup.findOne({ where: { HospitalGroupID: hospitalGroup_IDR } });
+    if (!group) {
+      const executionTime = `${Date.now() - start}ms`;
+      const errorCode = 1260;
+
+      logger.logWithMeta("error", "Invalid HospitalGroupID, not found in MasterDB", {
+        errorCode,
+        executionTime,
+        hospitalId: req.hospitalName,
+        apiName: req.originalUrl,
+        city: locationData?.city,
+        country: locationData?.country,
+        method: req.method,
+        userAgent: req.headers["user-agent"],
+        createdBy: req.username,
+      });
+      return res.status(400).json({ errorCode, message: "Invalid HospitalGroupID, not found in MasterDB" });
+    }
+
+    const hospitalid = await Hospital.findOne({ where: { HospitalID: hospital_IDR } });
+    if (!hospitalid) {
+      const executionTime = `${Date.now() - start}ms`;
+      const errorCode = 1260;
+
+      logger.logWithMeta("error", "Invalid HospitalID, not found in MasterDB", {
+        errorCode,
+        executionTime,
+        hospitalId: req.hospitalName,
+        apiName: req.originalUrl,
+        city: locationData?.city,
+        country: locationData?.country,
+        method: req.method,
+        userAgent: req.headers["user-agent"],
+        createdBy: req.username,
+        updatedBy: req.username
+      });
+      return res.status(400).json({ errorCode, message: "Invalid HospitalID, not found in MasterDB" });
+    }
+
+    const wardWiseData = await Wwca.findOne({ where: { Wwca_ID } });
+    if (!wardWiseData) {
+      logger.logWithMeta("error", "Ward wise cost addition not found", { Wwca_ID, hospitalDatabase, apiName: req.originalUrl });
+      return res.status(404).json({ errorCode: 1261, message: "Ward wise cost addition not found" });
+    }
+
+    await wardWiseData.update({
+      WwcatypeIDR,
+      typeEnum,
+      typeIDR,
+      costAddRate,
+      fromdate,
+      todate,
+      isActive,
+      isCurrectRate,
+      hospital_IDR,
+      hospitalGroup_IDR,
+      updatedBy: req.username,
+      UpdatedAt: new Date(),
+    });
+
+
+    const executionTime = `${Date.now() - start}ms`;
+    logger.logWithMeta("info", "Ward wise cost addition updated successfully", { Wwca_ID, hospitalDatabase, executionTime,city: locationData?.city,
+      country: locationData?.country, apiName: req.originalUrl });
+
+    res.status(200).json({
+      meta: { statusCode: 200, executionTime, hospitalDatabase },
+      data: { wardWiseData },
+    });
+
+  } catch (error) {
+    const executionTime = `${Date.now() - start}ms`;
+    const errorCode = 1262;
+
+    logger.logWithMeta("error", "Error updating ward wise cost addition", { errorCode, executionTime, hospitalDatabase, apiName: req.originalUrl, error: error.message });
+
+    res.status(500).json({
+      meta: { statusCode: 500, errorCode, executionTime, hospitalDatabase },
+      error: { message: "Error updating ward wise cost addition: " + error.message },
+    });
+  }
+}
+
+exports.deleteWwcaById = async (req, res) => {
+  const start = Date.now();
+  const hospitalDatabase = req.hospitalDatabase;
+  const { Wwca_ID } = req.params;
+
+  try {
+    const Wwca = require("../models/wardwiseCAModel.js")(req.sequelize);
+
+    const wardWiseData = await Wwca.findOne({ where: { Wwca_ID } });
+
+    if (!wardWiseData) {
+      logger.logWithMeta("error", "Ward wise cost addition not found", { Wwca_ID, hospitalDatabase, apiName: req.originalUrl });
+      return res.status(404).json({ errorCode: 1261, message: "Ward wise cost addition not found" });
+    } 
+
+    await wardWiseData.destroy();
+      const executionTime = `${Date.now() - start}ms`;
+      logger.logWithMeta("info", "Ward wise cost addition deleted successfully", { Wwca_ID, hospitalDatabase, executionTime, apiName: req.originalUrl });
+
+      res.status(200).json({
+        meta: { statusCode: 200, executionTime, hospitalDatabase },
+        message: "Ward wise cost addition deleted successfully",
+      });
+    
+  } catch (error) {
+    const executionTime = `${Date.now() - start}ms`;
+    const errorCode = 1263;
+    logger.logWithMeta("error", "Error deleting ward wise cost addition", { errorCode, executionTime, hospitalDatabase, apiName: req.originalUrl, error: error.message });
+    res.status(500).json({
+      meta: { statusCode: 500, errorCode, executionTime, hospitalDatabase },
+      error: { message: "Error deleting ward wise cost addition: " + error.message },
     });
   }
 }
--- routes/wardwiseCAroutes.js
+++ routes/wardwiseCAroutes.js
@@ -9,8 +9,8 @@ const Userverifiction = require('../validators/Accesstokenverify');
 
 router.post('/create_wardwca', validateJSONContentType, authenticate, Userverifiction, ensureSequelizeInstance, wardwcacontroller.createWwca);
 router.get('/get_wardwca', authenticate, Userverifiction, ensureSequelizeInstance, wardwcacontroller.getWwca);
-router.get('/get_wardwca/:id', authenticate, Userverifiction, ensureSequelizeInstance, wardwcacontroller.getWwcaById);
-// router.put('/put_ward/:ward_ID', authenticate,Userverifiction,ensureSequelizeInstance,wardwcacontroller.updateWard);
-// router.delete('/delete_ward/:ward_ID', authenticate,Userverifiction,ensureSequelizeInstance,wardwcacontroller.deleteWard);
+router.get('/get_wardwca/:Wwca_ID', authenticate, Userverifiction, ensureSequelizeInstance, wardwcacontroller.getWwcaById);
+router.put('/put_wardwca/:Wwca_ID', authenticate,Userverifiction,ensureSequelizeInstance,wardwcacontroller.updateWwcaById);
+router.delete('/delete_wardwca/:Wwca_ID', authenticate,Userverifiction,ensureSequelizeInstance,wardwcacontroller.deleteWwcaById);
 
 module.exports = router;
