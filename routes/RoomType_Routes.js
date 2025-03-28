6f012c3e0d11453f35a3d87dec41185254f0fd8d
aarzeal <168904553+aarzeal@users.noreply.github.com>
2025-03-28 10:25:16 +0530

    room type routes

--- routes/RoomType_Routes.js
+++ routes/RoomType_Routes.js
@@ -8,11 +8,11 @@ const validateTax = require('../validators/TaxValidators');
 const Userverification = require('../validators/Accesstokenverify');
 const validation = require("../validators/validation")
 
-router.post('/create-room-type', validateJSONContentType, authenticate, validation.validateRoomTypeRegister, Userverification, ensureSequelizeInstance, RoomType_Controller.createRoomType);
-router.get('/get-room-type', authenticate, Userverification, ensureSequelizeInstance, RoomType_Controller.getallroomtype);
-router.get('/get-room-type-by-id/:roomType_ID', authenticate, Userverification, ensureSequelizeInstance, RoomType_Controller.getRoomTypeById);
-router.put('/update-room-type-by-id/:roomType_ID', validateJSONContentType, Userverification, validation.validateRoomTypeUpdate, authenticate, ensureSequelizeInstance, RoomType_Controller.updateRoomType);
-router.delete('/delete-room-type-by-id/:roomType_ID', authenticate, Userverification, ensureSequelizeInstance, RoomType_Controller.deleteroomTypeById);
+router.post('/room-type', validateJSONContentType, authenticate, validation.validateRoomTypeRegister, Userverification, ensureSequelizeInstance, RoomType_Controller.createRoomType);
+router.get('/room-type', authenticate, Userverification, ensureSequelizeInstance, RoomType_Controller.getallroomtype);
+router.get('/room-type/:roomType_ID', authenticate, Userverification, ensureSequelizeInstance, RoomType_Controller.getRoomTypeById);
+router.put('/room-type/:roomType_ID', validateJSONContentType, Userverification, validation.validateRoomTypeUpdate, authenticate, ensureSequelizeInstance, RoomType_Controller.updateRoomType);
+router.delete('/room-type/:roomType_ID', authenticate, Userverification, ensureSequelizeInstance, RoomType_Controller.deleteroomTypeById);
 
 
 module.exports = router;
