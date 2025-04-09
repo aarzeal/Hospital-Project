85b9c12c1be5a0f44717b4b0b9328f9ecd3b8b10
aarzeal <sudhir.kale@aarzeal.com>
2025-04-09 16:04:35 +0530

    timetoseconds

--- util/methods.js
+++ util/methods.js
@@ -1,10 +1,21 @@
-const timeToSeconds=(timeStr)=> {
-    const timeParts = timeStr.split(":");
-    const hours = parseInt(timeParts[0]);
-    const minutes = parseInt(timeParts[1]);
-    const seconds = parseInt(timeParts[2]);
+// const timeToSeconds=(timeStr)=> {
+//     const timeParts = timeStr.split(":");
+//     const hours = parseInt(timeParts[0]);
+//     const minutes = parseInt(timeParts[1]);
+//     const seconds = parseInt(timeParts[2]);
 
+//     return (hours * 3600) + (minutes * 60) + seconds;
+// }
+
+// module.exports={timeToSeconds}
+
+const timeToSeconds = (timeStr) => {
+    const timeParts = timeStr.split(":");
+    const hours = timeParts[0] ? parseInt(timeParts[0]) : 0;
+    const minutes = timeParts[1] ? parseInt(timeParts[1]) : 0;
+    const seconds = timeParts[2] ? parseInt(timeParts[2]) : 0;
+
+    console.log("timeStr", minutes, seconds);
     return (hours * 3600) + (minutes * 60) + seconds;
 }
-
-module.exports={timeToSeconds}
+module.exports = { timeToSeconds }
