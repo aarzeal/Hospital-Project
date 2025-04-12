92b73b26904a11384ef8c7918d473b6d359594a2
aarzeal <sudhir.kale@aarzeal.com>
2025-04-12 18:16:57 +0530

    method

--- util/methods.js
+++ util/methods.js
@@ -10,12 +10,25 @@
 // module.exports={timeToSeconds}
 
 const timeToSeconds = (timeStr) => {
+    if (!timeStr || typeof timeStr !== 'string') return NaN;
+
     const timeParts = timeStr.split(":");
     const hours = timeParts[0] ? parseInt(timeParts[0]) : 0;
     const minutes = timeParts[1] ? parseInt(timeParts[1]) : 0;
     const seconds = timeParts[2] ? parseInt(timeParts[2]) : 0;
 
-    console.log("timeStr", minutes, seconds);
     return (hours * 3600) + (minutes * 60) + seconds;
-}
-module.exports = { timeToSeconds }
+};
+
+module.exports = { timeToSeconds };
+
+// const timeToSeconds = (timeStr) => {
+//     const timeParts = timeStr.split(":");
+//     const hours = timeParts[0] ? parseInt(timeParts[0]) : 0;
+//     const minutes = timeParts[1] ? parseInt(timeParts[1]) : 0;
+//     const seconds = timeParts[2] ? parseInt(timeParts[2]) : 0;
+
+//     console.log("timeStr", minutes, seconds);
+//     return (hours * 3600) + (minutes * 60) + seconds;
+// }
+// module.exports = { timeToSeconds }
