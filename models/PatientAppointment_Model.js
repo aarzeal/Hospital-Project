b2629a6a2ce2fad882b6e53a52ae1767ef736b32
aarzeal <168904553+aarzeal@users.noreply.github.com>
2025-04-24 11:29:14 +0530

    patient Contact Number validation changed

--- models/PatientAppointment_Model.js
+++ models/PatientAppointment_Model.js
@@ -103,7 +103,7 @@ module.exports=(sequelize)=> {
                 type: DataTypes.STRING,
                 allowNull: false,
                 validate: {
-                  is: /^[0-9]{10}$/,
+                  is: /^[0-9]$/,
                   notEmpty: true 
                 }
             },
